8cf21168596a3344fa68197181564b7d5fc183cd
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-01-18 15:27:18 +0800

    Function definition for address to distance.

--- Address_to_distance.py
+++ Address_to_distance.py
@@ -1,6 +1,6 @@
-# From a CSV file,
-#
-#
-#
-#
-#
+# From a CSV file, read each entry for (username : postal_code). Create dictionary of (username : postal_code)
+# From garden_address and postal_code, get distance between postal_code and garden.
+# Create and return dictionary of (username : distance)
+
+def address_to_distance() -> dict(str, int):
+    return {"user1": 3.1415, "user2": 1.618}
