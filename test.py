ca7728dd33b063c778947455cea899f0f319f2e1
Goh Wei Han <weihan.goh.2021@scis.smu.edu.sg>
2023-01-19 17:33:03 +0800

    finished ballot_selector

--- test.py
+++ test.py
@@ -36,8 +36,5 @@ def ballot_selector(user_dist, num_plots):
     
     return output
 
-# This function takes in a dictionary of users and their distance from an allotment garden user_dist = {"user1": 20.11, "user2": 3.52, "user3": 1.26}.
-# The maximum number of winners is the num_plots = 1
-# The dist_weight is the weight given to each user based on distance. dist_weight = {1: 10, 2: 5, 5: 3}
-# This means (dist < 1; 10 points), (1 <= dist < 2; 5 points), (2 <= dist < 5; 3 points), (dist >= 5; 1 point). This also implies that the minimum num of points a user can get is 1 point.
-# After randomly selecting num_plots winners, return the list of winners as a set.
+user_dist = {'a':.4, 'b':6, 'c':4.1, 'd':5.1, 'e':0.9, 'f':1.01, 'g':3.2, 'h':4.99, 'i':5.0, 'j':100}
+print(ballot_selector(user_dist, 5))
