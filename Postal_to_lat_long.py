f38dd190ece23022a8ca1c795cc2c61630033c7e
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-02-01 15:45:28 +0800

    Complete postal to lat long.

--- Postal_to_lat_long.py
+++ Postal_to_lat_long.py
@@ -1,4 +1,4 @@
-# from FileIO.File_read_excel import read_excel_data_to_dataframe
+import math
 import pgeocode
 import pandas as pd
 
@@ -7,18 +7,36 @@ import pandas as pd
 # From dataframe, call pgeocode api to get lat and long from postal code.
 # Add the lat and long columns to the dataframe. Return dataframe containing 4 columns (username: str, postal_code: str, lat: double, long: double)
 
+nomi = pgeocode.Nominatim('sg')
+
 
 def postal_to_lat_long(user_postal_code_df: pd.DataFrame) -> pd.DataFrame:
+    lat_col = []
+    long_col = []
+
     for index, row in user_postal_code_df.iterrows():
-        user_postal_code_df['Latitude'], user_postal_code_df['Longitude'] = get_lat_long(
-            row.get('Postal Code'))
+        lat, long = get_lat_long(row.get('Postal Code'))
+        lat_col.append(lat)
+        long_col.append(long)
+
+    user_postal_code_df = user_postal_code_df.assign(
+        Latitude=pd.Series(lat_col).values)
+    user_postal_code_df = user_postal_code_df.assign(
+        Longitude=pd.Series(long_col).values)
+    # print(user_postal_code_df)
     return user_postal_code_df
 
 
-def get_lat_long(postal_code: str) -> tuple:
-    nomi = pgeocode.Nominatim('sg')
-    df = nomi.query_postal_code(postal_code)
-    return (df.get('latitude'), df.get('longitude'))
+def get_lat_long(postal_code: int) -> tuple:
+    pc_length = int(math.log10(postal_code)) + 1
+    valid_pc = (6 - pc_length) * "0" + \
+        str(postal_code) if pc_length < 6 else str(postal_code)
+
+    df = nomi.query_postal_code(valid_pc)
+    lat, long = df.get('latitude'), df.get('longitude')
+    if math.isnan(lat) or math.isnan(long):
+        lat, long = ('1.3521', '103.8198')  # Defaults to centre of SG if NaN
+    return (lat, long)
 
 
-print(get_lat_long('679521'))
+# print(get_lat_long('679521'))
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import os
 from dotenv import load_dotenv
+import asyncio
 
 from FileIO.File_read_excel import read_excel_data_to_dataframe
+from Postal_to_lat_long import postal_to_lat_long
 
 # TODO: Write main method calling all other functions.
 load_dotenv()
 
-raw_df = read_excel_data_to_dataframe(os.getenv("FILENAME"))
 
+raw_df = read_excel_data_to_dataframe(os.getenv("FILENAME"))
+valid_df = raw_df
+
+# Lat and long not found in df. Need to memoise.
 if raw_df.get('Latitude') is None or raw_df.get('Longitude') is None:
-    print("Should run postal to lat long")
+    valid_df = postal_to_lat_long(raw_df)
+    valid_df.to_excel("output.xlsx", index=False)
+
+
+# print(valid_df)
