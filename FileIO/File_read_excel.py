508b159fba9cfbb7fec852952ab4d68e5e79c89a
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-02-03 02:18:05 +0800

    CSV and colab file path.

--- FileIO/File_read_excel.py
+++ FileIO/File_read_excel.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def read_excel_data_to_dataframe(filename: str) -> pd.DataFrame:
     try:
-        return pd.read_csv('Excel/' + filename)
+        return pd.read_csv('/content/gdrive/MyDrive/Colab Notebooks/BallotAlgo/Excel/' + filename)
     except FileNotFoundError as e:
         print(f"File not found\n{e}")
     except Exception as e:
